2007933360ce228af9760bcd3aeb94ae5f08532f
Vy Phan <vywebber@gmail.com>
2020-12-21 11:28:28 +0700

    add filter url

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     if (localStorage.getItem(Global.JWT_KEY) == null) {
+
       this.router.navigate(['/login']);
     }
   }
